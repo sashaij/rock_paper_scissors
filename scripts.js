9ce2fae3d9f166d41368911c6dc08845b6a77bbb
sashaij <o.zagorodniuk@outlook.com>
2023-08-27 10:54:37 +0300

    add game logic algorythm completed

--- scripts.js
+++ scripts.js
@@ -1,23 +1,39 @@
-const getComputerChoice = function () {
+function getComputerChoice () {
     //generate random number
     let randomNumber = ((Math.random()) * 10).toFixed(2);
-    console.log(randomNumber);
     //attach all three moves to a certain value
     if (randomNumber > 0 && randomNumber <= 3.33) {
-        console.log('Rock');
+        return 'Rock';
     } else if (randomNumber > 3.33 && randomNumber <= 6.66) {
-        console.log('Paper');
+        return 'Paper';
     } else {
-        console.log('Scissors');
+        return 'Scissors';
     }
     //display the value
 }
+
+const computerPick = getComputerChoice();
+
    //win-loss function
-const playRound = function (playerMove) {
-    if (playerMove === getComputerChoice) {
-        console.log('its a tie');
-}
-}
-getComputerChoice();
+const playRound = function (playerMove, computerMove) {
 
+    if (computerMove === playerMove) {
+        console.log('it\'s a tie');
+} else if (playerMove === 'Rock' && computerMove === 'Scissors'){
+    console.log(`Computer picked ${computerMove}. You picked ${playerMove}. You win`);
+} else if (playerMove === 'Rock' && computerMove === 'Paper') {
+   console.log(`Computer picked ${computerMove}. You picked ${playerMove}. You lose.`); 
+} else if (playerMove === 'Paper' && computerMove === 'Scissors' ) {
+    console.log(`Computer picked ${computerMove}. You picked ${playerMove}. You lose.`);
+} else if (playerMove === 'Paper' && getComputerChoice === 'Rock' ) {
+    console.log(`Computer picked ${computerMove}. You picked ${playerMove}. You win.`);
+} else if (playerMove === 'Scissors' && computerMove === 'Rock' ) {
+    console.log(`Computer picked ${computerMove}. You picked ${playerMove}. You lose.`);
+} else if (playerMove === 'Scissors' && computerMove === 'Paper' ) {
+    console.log(`Computer picked ${computerMove}. You picked ${playerMove}. You win.`);
+} else {
+    console.log('Wrong input.');
+} 
+}
 
+playRound('Rock', computerPick);
